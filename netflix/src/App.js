7882857c8d7c80e43a9733e9c07edf0fb1d5d25d
hasanalrawaqa <hasanalrawaqa@gmail.com>
2023-04-10 20:51:21 +0000

    19

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Home from './components/Home/Home';
 import { Routes, Route } from 'react-router-dom';
+import FavList from './components/FavList /FavList ';
+import NavBar from './components/Navbar/NavBar';
 function App() {
   return (
     <>
+    <NavBar />
       <Routes>
         <Route path="/trending" component={<Home />} />
+        <Route path="/favlist" element={< FavList />} />
       </Routes>
     </>
   );
